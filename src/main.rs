fadb1a5886f29a0df0a8c1272da91720f56a3e4d
Michael Vlaar <michael@vlaar.it>
2024-08-13 16:04:40 +0200

    feat: HTTP/2 htmx javascript push on index

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,33 @@ mod routes;
 mod models;
 mod utils;
 
-use actix_web::{web, App, HttpServer};
+use actix_web::{web, App, HttpServer, HttpRequest, HttpResponse, Responder};
 use rust_embed::RustEmbed;
 use tera::Tera;
+use mime_guess::from_path;
 
 #[derive(RustEmbed)]
 #[folder = "templates/"]
 struct Templates;
+
+#[derive(RustEmbed)]
+#[folder = "assets/"]
+struct Assets;
+
+async fn serve_asset(req: HttpRequest) -> impl Responder {
+    let path: String = req.match_info().query("filename").parse().unwrap();
+
+    match Assets::get(&path) {
+        Some(content) => {
+            let body = content.data;
+            let mime_type = from_path(path).first_or_octet_stream();
+            HttpResponse::Ok()
+                .content_type(mime_type.as_ref())
+                .body(body)
+        },
+        None => HttpResponse::NotFound().body("Asset not found"),
+    }
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -24,6 +44,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .app_data(web::Data::new(tera.clone()))
             .configure(routes::init)
+            .route("/assets/{filename:.*}", web::get().to(serve_asset))
     })
     .bind("0.0.0.0:80")?
     .run()
--- src/routes/index.rs
+++ src/routes/index.rs
@@ -26,12 +26,7 @@ pub async fn index(
 
     let rendered = tmpl.render(template, &ctx).unwrap();
     HttpResponse::Ok()
-        .insert_header(("Link", "<https://unpkg.com/htmx.org@2.0.1>; rel=preload; as=script"))
-        .insert_header(("Link", "<https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css>; rel=preload; as=style"))
-        .insert_header(("Link", "<https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css>; rel=preload; as=style"))
-        .insert_header(("Link", "<https://fonts.googleapis.com/icon?family=Material+Icons>; rel=preload; as=style"))
-        .insert_header(("Link", "<https://cdn.jsdelivr.net/npm/flatpickr>; rel=preload; as=script"))
-        .insert_header(("Link", "<data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%221em%22 font-size=%2280%22>&#128747;</text></svg>>; rel=preload; as=image"))        .content_type("text/html")
+        .insert_header(("Link", "</assets/htmx.min.js>; rel=preload; as=script"))
         .body(rendered)
 }
 
